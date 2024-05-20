e026277b87090b9a59d3189503c23b0c42dab36e
prachit082 <panditprachit20@gmail.com>
2024-05-20 19:40:56 +0530

    Changed URL for deploying

--- app/components/page-data.tsx
+++ app/components/page-data.tsx
@@ -8,7 +8,7 @@ export function PageData({ data }: { data: any }) {
       <main>
         <a
           target="_blank"
-          href="https://chatg.pt/weather"
+          href="https://vercel.com/new/clone?demo-title=WeatherGPT%20%E2%80%93%C2%A0ChatGPT%20Plugin%20Starter&demo-description=The%20ultimate%20ChatGPT%20Plugin%20starter%20template.%20WeatherGPT%20is%20a%20ChatGPT%20Plugin%20to%20get%20the%20weather%20of%20any%20given%20location.&demo-url=https%3A%2F%2Fweatherai.vercel.app%2F&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F7Ft2fS9gtS9XtxUNVJJENP%2F04a68855bda0524982d3c15cbd2628ac%2FCleanShot_2023-04-13_at_12.37.09.png&project-name=WeatherGPT%20%E2%80%93%C2%A0ChatGPT%20Plugin%20Starter&repository-name=Weather-Ai&repository-url=https%3A%2F%2Fgithub.com%2Fprachit082%2FWeather-Ai&from=templates&skippable-integrations=1&env=WEATHER_API_KEY&envDescription=Get%20your%20WeatherAPI%20key%20here%3A&envLink=https%3A%2F%2Fwww.weatherapi.com%2F&teamCreateStatus=hidden"
           rel="noreferrer"
           className="pill"
         >
